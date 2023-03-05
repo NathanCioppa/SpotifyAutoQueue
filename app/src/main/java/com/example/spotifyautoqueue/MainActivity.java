00b8b0e2f08c1f094f3f162678a6cbf617f7edec
NathanCioppa <nathan.cioppa@gmail.com>
2023-03-05 11:39:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/spotifyautoqueue/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.spotifyautoqueue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.types.Track;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    final String CLIENT_ID = "1435a3ff53c84332b17feb17f4cb0cb8";
+    final String REDIRECT_URI = "https://nathancioppa.github.io/spotify-app.html";
+    SpotifyAppRemote spotifyAppRemote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+            @Override
+            public void onConnected(SpotifyAppRemote sam) {
+                spotifyAppRemote = sam;
+                Log.d("MainActivity", "Connected");
+
+                connected();
+            }
+
+            @Override
+            public void onFailure(Throwable error) {
+                Log.e("MainActivity", error.getMessage(), error );
+            }
+        });
+    }
+
+    private void connected() {
+        spotifyAppRemote.getPlayerApi().subscribeToPlayerState().setEventCallback(playerState -> {
+            final Track track = playerState.track;
+            if (track != null) {
+                Log.d("MainActivity", track.name + " by " + track.artist.name);
+            } else {
+                Log.d("MainActivity", "No track is playing");
+            }
+        });
+    }
+
+    public void getQueue() {
+
+    }
+
+    public void openSettingsActivity(View button) {
+        Intent openSettings = new Intent(this,SettingsActivity.class);
+        startActivity(openSettings);
+    }
+
+    public void openCreateGroupActivity(View button) {
+        Intent openCreateGroup = new Intent(this, CreateGroupActivity.class);
+        startActivity(openCreateGroup);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(spotifyAppRemote);
+        Log.d("onStop", "Disconnected");
+    }
+}
