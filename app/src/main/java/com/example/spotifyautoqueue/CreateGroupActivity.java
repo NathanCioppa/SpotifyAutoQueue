c605db2e15e069b4de62881053f39858db80afbd
NathanCioppa <nathan.cioppa@gmail.com>
2023-03-17 21:22:08 -0400

    seached tracks show in parent recycler view
    honestly cant believe that worked basically first try

--- app/src/main/java/com/example/spotifyautoqueue/CreateGroupActivity.java
+++ app/src/main/java/com/example/spotifyautoqueue/CreateGroupActivity.java
@@ -1,6 +1,8 @@
 package com.example.spotifyautoqueue;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,10 +16,12 @@ import java.util.ArrayList;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
+    RecyclerView parentSearchRecycler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+        parentSearchRecycler = findViewById(R.id.searchedParentTrackRecycler);
     }
 
     static ArrayList<SearchItem> parentSearches;
@@ -29,6 +33,13 @@ public class CreateGroupActivity extends AppCompatActivity {
 
         try{
             parentSearches = searchSpotify.execute().get();
+
+            if (parentSearches != null) {
+                ParentTrackSearchesAdapter parentAdapter = new ParentTrackSearchesAdapter(this, parentSearches);
+                parentSearchRecycler.setAdapter(parentAdapter);
+                parentSearchRecycler.setLayoutManager(new LinearLayoutManager(this));
+            }
+
         } catch (Exception error) {
             error.printStackTrace();
         }
