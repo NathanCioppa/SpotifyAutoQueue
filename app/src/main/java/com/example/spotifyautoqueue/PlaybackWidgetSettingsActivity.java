c9b078e3d5f128b75721577b0649d233529a1726
NathanCioppa <nathan.cioppa@gmail.com>
2023-04-05 22:02:03 -0400

    I want to scream slightly less than I did earlier

--- app/src/main/java/com/example/spotifyautoqueue/PlaybackWidgetSettingsActivity.java
+++ app/src/main/java/com/example/spotifyautoqueue/PlaybackWidgetSettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.spotifyautoqueue;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.view.WindowCompat;
 
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
@@ -9,22 +10,21 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RadioButton;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
 public class PlaybackWidgetSettingsActivity extends AppCompatActivity {
 
     int widgetId;
-    TextView exampleName;
-    TextView exampleArtist;
-
     int[] configData;
     static int textColor = Color.WHITE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        exampleName = findViewById(R.id.exampleName);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -36,20 +36,20 @@ public class PlaybackWidgetSettingsActivity extends AppCompatActivity {
         }
 
         configData = new int[3];
+
         setContentView(R.layout.activity_playback_widget_settings);
+        updatePreview(UPDATE_ALL);
+        selectCurrentChoices();
     }
 
     public void setTextBlack(View button) {
         textColor = Color.BLACK;
-        try{
-            exampleName.setTextColor(Color.BLACK);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        updatePreview(TEXT_COLOR_KEY);
     }
 
     public void setTextWhite(View button) {
         textColor = Color.WHITE;
+        updatePreview(TEXT_COLOR_KEY);
     }
 
     public void finishConfig(View button) {
@@ -62,5 +62,27 @@ public class PlaybackWidgetSettingsActivity extends AppCompatActivity {
         Intent resultValue = new Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
         setResult(RESULT_OK, resultValue);
         finish();
+    }
+
+    final int UPDATE_ALL = 0;
+    final int TEXT_COLOR_KEY = 1;
+    final int BUTTON_COLOR_KEY = 2;
+    final int BACKGROUND_COLOR_KEY = 3;
+    public void updatePreview(int key) {
+        if(key == TEXT_COLOR_KEY || key == UPDATE_ALL) {
+            ((TextView)findViewById(R.id.exampleConfigWidgetName)).setTextColor(textColor);
+            ((TextView)findViewById(R.id.exampleConfigWidgetArtist)).setTextColor(textColor);
+
+        }
+        //if (key == BUTTON_COLOR_KEY || key == UPDATE_ALL) {
+
+        //}
+    }
+
+    public void selectCurrentChoices() {
+        if(textColor == Color.WHITE)
+            ((RadioButton)findViewById(R.id.buttonTextWhite)).performClick();
+        else
+            ((RadioButton)findViewById(R.id.buttonTextBlack)).performClick();
     }
 }
